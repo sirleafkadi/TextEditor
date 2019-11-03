dd40d18c83cf05a5c486ebe5a8a047c8f6655cc7
Tech <Tech@DESKTOP-0ILHQEM>
2019-11-03 10:47:38 -0600

    Creating Link List

--- MOOCTextEditor/src/textgen/MyLinkedList.java
+++ MOOCTextEditor/src/textgen/MyLinkedList.java
@@ -16,7 +16,11 @@ public class MyLinkedList<E> extends AbstractList<E> {
 
 	/** Create a new empty LinkedList */
 	public MyLinkedList() {
-		// TODO: Implement this method
+		this.size = 0;
+		this.head = new LLNode<E>();
+		this.tail = new LLNode<E>();
+		this.head.next = this.tail;
+		this.tail.prev = this.head;
 	}
 
 	/**
@@ -26,15 +30,35 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public boolean add(E element ) 
 	{
 		// TODO: Implement this method
-		return false;
+		if (element == null) {
+			throw new NullPointerException("Index is empty");
+		}
+		LLNode<E> newnode = new LLNode<E>(element);
+		LLNode<E> stackreferernce = tail.prev;
+		stackreferernce.next = newnode;
+		newnode.prev = stackreferernce;
+		newnode.next = tail;
+		tail.prev = newnode;
+		size++;
+		return true;
 	}
 
 	/** Get the element at position index 
 	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
 	public E get(int index) 
 	{
+		
+		
 		// TODO: Implement this method.
-		return null;
+		if (index < 0 || index > size-1 ) {
+			throw new IndexOutOfBoundsException("Wrong input size!");
+		}
+		LLNode<E> search = head;
+		for (int i = 0; i <= index; i++) {
+			search = search.next;
+		}
+		E data = search.data;
+		return data;
 	}
 
 	/**
@@ -45,6 +69,24 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public void add(int index, E element ) 
 	{
 		// TODO: Implement this method
+		if (element == null) {
+			throw new NullPointerException("Wrong input size!");
+		}
+		if ((index < 0 || index > size - 1) && (index != 0 || size != 0)) {
+			throw new IndexOutOfBoundsException("Wrong input size!");
+		}
+		
+		LLNode<E> newnode = new LLNode<E>(element);
+		LLNode<E> search = head;
+		for (int i = 0; i <= index; i++) {
+			search = search.next;
+		}
+		LLNode<E> stackreferenceprev = search.prev;
+		stackreferenceprev.next = newnode;
+		newnode.prev = stackreferenceprev;
+		newnode.next = search;
+		search.prev = newnode;
+		size++;
 	}
 
 
@@ -52,7 +94,7 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public int size() 
 	{
 		// TODO: Implement this method
-		return -1;
+		return size;
 	}
 
 	/** Remove a node at the specified index and return its data element.
@@ -64,7 +106,22 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public E remove(int index) 
 	{
 		// TODO: Implement this method
-		return null;
+		if (index < 0 || index > size - 1) {
+			throw new IndexOutOfBoundsException("Wrong input size!");
+		}
+		LLNode<E> search = head;
+		for (int i = 0; i <= index; i++) {
+			search = search.next;
+		}
+	
+		LLNode<E> prev = search.prev;
+		LLNode<E> next = search.next;
+	
+		prev.next = next;
+		next.prev = prev;
+		size--;
+		E value = search.data;
+		return value;
 	}
 
 	/**
@@ -77,7 +134,18 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public E set(int index, E element) 
 	{
 		// TODO: Implement this method
-		return null;
+		if (index < 0 || index > size - 1) {
+			throw new IndexOutOfBoundsException("Wrong input size!");
+		}
+		if (element == null) {
+			throw new NullPointerException("Wrong input size!");
+		}
+		LLNode<E> search = head;
+		for (int i = 0; i <= index; i++) {
+			search = search.next;
+		}
+		search.data = element;
+		return element;
 	}   
 }
 
@@ -89,12 +157,17 @@ class LLNode<E>
 
 	// TODO: Add any other methods you think are useful here
 	// E.g. you might want to add another constructor
-
+	public LLNode()
+	{
+		this.data = null;
+		this.prev = null;
+		this.next = null;
+	}
+	
 	public LLNode(E e) 
 	{
 		this.data = e;
 		this.prev = null;
 		this.next = null;
 	}
-
 }
